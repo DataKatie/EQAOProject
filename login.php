cf84b57ad5eac1f7c1e55819d0bc60a3aa9c9175
Katie Sullivan <katie.m.sullivan@gmail.com>
2016-11-17 08:09:18 -0500

    login

--- login.php
+++ login.php
@@ -42,7 +42,70 @@ if(isset($_POST['btnsubmit']))
 
 }
 ?>
-<html>
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->  
+<head>
+    <title>Edu-Tech</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">    
+    <link rel="shortcut icon" href="favicon.ico">  
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,500,500italic,700,700italic,900,900italic,300italic,300' rel='stylesheet' type='text/css'> 
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100' rel='stylesheet' type='text/css'>
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">   
+    <!-- Plugins CSS -->    
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/plugins/flexslider/flexslider.css">
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="assets/css/styles-2.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <style>
+        footer ul.social.list-inline li a {
+            display: block;
+            height: 50px;
+            width: 50px;
+            line-height: 50px;
+            font-size: 25px;
+            border-radius: 100%;
+            color: white;
+            text-align: center;
+        }
+
+        .profile-picture {
+          display: inline-block;
+          width: 150px;
+          height: 150px;
+          border-radius: 50%;
+          background-repeat: no-repeat;
+          background-position: center center;
+          background-size: cover;
+        }
+
+        footer ul.social.list-inline { margin: 0; text-align: center;}
+        footer ul.social.list-inline li.social-github a { background-color: #333333; }
+        footer ul.social.list-inline li.social-github a:hover { background-color: white; }
+        footer ul.social.list-inline li.social-twitter a { background-color: #1da1f2; }
+        footer ul.social.list-inline li.social-twitter a:hover { background-color: white; }
+        footer ul.social.list-inline li.social-facebook a { background-color: #3b5998; }
+        footer ul.social.list-inline li.social-facebook a:hover { background-color: white; }
+        footer ul.social.list-inline li.social-google-plus a { background-color: #dd4b39; }
+        footer ul.social.list-inline li.social-google-plus a:hover { background-color: white; }
+        footer .footer { text-align: center; padding: 50px 0; }
+    </style>
+</head> 
+
+<body class="home-page">  
 	<form method="post" action="login.php">
 		<p> User Name: <input type="text" name="userName" value="<?php if(isset($_COOKIE['userName'])) { echo $_COOKIE['userName']; } ?>"/> </p>
 		<p> Password: <input type="password" name="password" value="<?php if(isset($_COOKIE['password'])) { echo $_COOKIE['password']; } ?>"/></p>
@@ -50,6 +113,7 @@ if(isset($_POST['btnsubmit']))
 			<input type="submit" name="btnsubmit" value="Submit" />
 	</form>
 	<a href="forgotpassword.php"> Forgot my password </a>
+</body>
 </html>    
 <?php
 
